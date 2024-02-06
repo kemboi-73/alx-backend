266943b21211f44bf7f09309255a8de017dd922c
kemboi-73 <kemboimichael52@gmail.com>
2024-02-06 08:47:21 -0800

    forcing locale with webs urls

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""A simple flask app
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# configure the flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """returns the locale"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+# babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """render the template"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
